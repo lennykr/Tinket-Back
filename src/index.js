783ab78bbb0291fb773fcdc67d5d3361054cf62c
Lenny Kraaijenhof <hi@lenny.kr>
2019-12-10 09:38:39 +0100

    Update dependencies and index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 const cors = require('cors');
+
 require('dotenv').config();
+
+global.moment = require('moment');
+moment.locale('nl-be');
+
+const { version: currentVersion } = require('../package.json');
+
+const app = express();
 const router = require('./routes');
 const port = 3000;
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
+
+app.use(express.static('public'));
+
 app.use(router);
+
+app.get('/', function (req, res, next) {
+    res.send(`API Version: ${ currentVersion }`);
+});
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
     .then(() => {
--- src/models/Assignment.js
+++ src/models/Assignment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const AssignmentSchema = mongoose.Schema({
     title: {
