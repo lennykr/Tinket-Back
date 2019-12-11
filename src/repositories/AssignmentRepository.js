c16eb1ad6d78653b6ed5c157f5cf3b27aaac9bbf
Sleeyax <yourd3veloper@gmail.com>
2019-12-11 23:03:20 +0100

    Populate requiredSkills

--- src/repositories/AssignmentRepository.js
+++ src/repositories/AssignmentRepository.js
@@ -1,11 +1,13 @@
+// this import is required for .populate() to work
+const Skill = require('../models/Skill');
 const Assignment = require('../models/Assignment');
 const BaseRepository = require('./BaseRepository');
 
 class AssignmentRepository extends BaseRepository {
     constructor() {super(Assignment);}
 
-    readAll(){
-        return this.model.find({});
+    readAll() {
+        return this.model.find({}).populate('requiredSkills');
     }
 }
 
