bc654eac5819ce8ed37386bd6a8d32c170680d5e
Sleeyax <yourd3veloper@gmail.com>
2019-12-12 15:46:22 +0100

    Fix bugs

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
     // ('!!' -> converts the value to false if it's null)
     if (!!req.user.isAdmin === false)
         res.status(403).send({ message: 'Privileges required.' });
-
-    next();
+    else
+        next();
 };
--- src/middleware/company.js
+++ src/middleware/company.js
@@ -2,6 +2,6 @@ module.exports = async (req, res, next) => {
     // only allow companies to access this endpoint
     if (req.user.companyProfile == null)
         res.status(403).send({message: 'Invalid profile type.'});
-
-    next();
+    else
+        next();
 };
