4ae05958118c2c382fe4580642d431ae8de3c32b
Sleeyax <yourd3veloper@gmail.com>
2019-12-14 20:19:20 +0100

    Fix model

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const moderate = require('./_moderate');
 
 const reviewSchema = mongoose.Schema({
@@ -23,7 +24,7 @@ const reviewSchema = mongoose.Schema({
         type: Number,
         default: 0
     },
-    
+
     ...moderate
 });
 
